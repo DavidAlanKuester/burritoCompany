8a1f945a65c6b49fd453402b728d33633c02a1fb
David Küster <davidalankuester@yahoo.de>
2020-10-03 23:19:13 +0200

    Implemented Main Page CSS (Header, Order, Footer sections), created JSON Menu, started for each loop with salad

--- app.js
+++ app.js
@@ -1,1 +1,35 @@
+function init() {
+    updateDishes()
+}
 
+function updateDishes() {
+    salads.forEach(function (salad) {
+        let dishDiv = 'dish-div';
+        let titleDiv = 'dish-div-title-div';
+        let descriptionDiv = 'dish-div-description-div';
+        let variationDiv = 'dish-div-variation-div';
+        let priceDiv = 'dish-div-price-div';
+        let btnDiv = 'dish-div-btn-div';
+        let plusImg = 'plus-img';
+        let htmlContent = `
+    <div id="salad-${salad.id}" class="${dishDiv}">
+    <div class="${titleDiv}">
+    <h3>${salad.title}</h3>
+    </div>
+    <div class="${descriptionDiv}">
+        <p>${salad.description}</p>
+    </div>
+    <div class="${variationDiv}">
+        <p>${salad.variation}</p>
+    </div>
+    <div class="${priceDiv}">
+        <p>${salad.price}€</p>
+    </div>
+    <div class="${btnDiv}">
+        <img class="${plusImg}" src="./img/plus.png">
+    </div>
+    </div>
+    `;
+        document.getElementById('salads-div').insertAdjacentHTML("beforeend", htmlContent);
+    });
+}
--- menu.js
+++ menu.js
@@ -1,33 +1,63 @@
 
-
 let popular  = [
     {
         title: '', description: '' 
     }
 ];
 
-
 let salads = [
     {
-        title: '', description: '' 
-    }
-];
-
-let salads = [
+        title: 'Aztec Chicken Salad', 
+        description: 'Fresh salad with mixed vegetables and juicy slices of chicken breast.',
+        variation: 'Variatons: Chipotle-tomato sauce, Habanero sauce, Avocado sauce, more meat',
+        price: '6,49',
+        id: '1'
+    },
     {
-        title: '', description: '' 
+        title: 'Mexican Beef Salad', 
+        description: 'Fresh salad with mixed vegetables and juicy slices of Beef.',
+        variation: 'Variatons: Chipotle-tomato sauce, Habanero sauce, Avocado sauce, more meat',
+        price: '6,99',
+        id: '2'
+    },
+    {
+        title: 'Mayan Avocado Salad', 
+        description: 'Fresh salad with mixed vegetables and lemon juiced avocado',
+        variation: 'Variatons: Chipotle-tomato sauce, Habanero sauce, more avoacado',
+        price: '6,99',
+        id: '3'
+    },
+    {
+        title: 'Veggi Salad', 
+        description: 'Fresh salad with mixed vegetables, eggs and cheese',
+        variation: 'Variatons: Chipotle-tomato sauce, Habanero sauce, Avocado sauce, more cheese or eggs',
+        price: '5,99',
+        id: '4'
+    },
+    {
+        title: 'Vegan Salad', 
+        description: 'Fresh salad with mixed vegetables',
+        variation: 'Variatons: Chipotle-tomato sauce, Habanero sauce, add avoacado',
+        price: '5,99',
+        id: '5'
     }
 ];
 
 let burritos = [
     {
-        title: '', description: '' 
+        title: 'Mexican Beef Salad', 
+        description: 'Fresh salad with mixed vegetables and juicy slices of Beef.',
+        variation: 'Variatons: Chipotle-tomato sauce, Habanero sauce, Avocado sauce, more meat',
+        price: '6,99'
     }
 ];
 
 let bowls = [
     {
-        title: '', description: ''   
+        title: 'Mayan Avocado Salad', 
+        description: 'Fresh salad with mixed vegetables and lemon juiced avocado',
+        variation: 'Variatons: Chipotle-tomato sauce, Habanero sauce, more avoacado',
+        price: '6,99'
     }
 ];
 
