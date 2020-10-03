67032665e0d7e50245315e975e0b8db9d73f5a1d
David Küster <davidalankuester@yahoo.de>
2020-10-03 19:24:44 +0200

    first commit

---
+++ menu.js
@@ -0,0 +1,50 @@
+
+
+let popular  = [
+    {
+        title: '', description: '' 
+    }
+];
+
+
+let salads = [
+    {
+        title: '', description: '' 
+    }
+];
+
+let salads = [
+    {
+        title: '', description: '' 
+    }
+];
+
+let burritos = [
+    {
+        title: '', description: '' 
+    }
+];
+
+let bowls = [
+    {
+        title: '', description: ''   
+    }
+];
+
+let vegan = [
+    {
+        title: '', description: '' 
+    }
+];
+
+let desserts = [
+    {
+        title: '', description: '' 
+    }
+];
+
+let drinks = [
+    {
+        title: '', description: '' 
+    }
+];
