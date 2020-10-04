d06a682645b1724fceba106d0665775f0c1e8ca2
David Küster <davidalankuester@yahoo.de>
2020-10-04 19:03:19 +0200

    added Burritos and Bowls from JSON(MENU) to Main page

--- app.js
+++ app.js
@@ -3,15 +3,15 @@ function init() {
 }
 
 function updateDishes() {
+    let dishDiv = 'dish-div';
+    let titleDiv = 'dish-div-title-div';
+    let descriptionDiv = 'dish-div-description-div';
+    let variationDiv = 'dish-div-variation-div';
+    let priceDiv = 'dish-div-price-div';
+    let btnDiv = 'dish-div-btn-div';
+    let plusImg = 'plus-img';
     salads.forEach(function (salad) {
-        let dishDiv = 'dish-div';
-        let titleDiv = 'dish-div-title-div';
-        let descriptionDiv = 'dish-div-description-div';
-        let variationDiv = 'dish-div-variation-div';
-        let priceDiv = 'dish-div-price-div';
-        let btnDiv = 'dish-div-btn-div';
-        let plusImg = 'plus-img';
-        let htmlContent = `
+        let saladContent = `
     <div id="salad-${salad.id}" class="${dishDiv}">
     <div class="${titleDiv}">
     <h3>${salad.title}</h3>
@@ -30,6 +30,55 @@ function updateDishes() {
     </div>
     </div>
     `;
-        document.getElementById('salads-div').insertAdjacentHTML("beforeend", htmlContent);
+        document.getElementById('salads-div').insertAdjacentHTML("beforeend", saladContent);
+    });
+    burritos.forEach(function (burrito) { 
+        let burritoContent = `
+        <div id="burrito-${burrito.id}" class="${dishDiv}">
+        <div class="${titleDiv}">
+        <h3>${burrito.title}</h3>
+        </div>
+        <div class="${descriptionDiv}">
+            <p>${burrito.description}</p>
+        </div>
+        <div class="${variationDiv}">
+            <p>${burrito.variation}</p>
+        </div>
+        <div class="${priceDiv}">
+            <p>${burrito.price}€</p>
+        </div>
+        <div class="${btnDiv}">
+            <img class="${plusImg}" src="./img/plus.png">
+        </div>
+        </div>
+        `;
+        document.getElementById('burritos-div').insertAdjacentHTML("beforeend", burritoContent);
+
+    });
+
+    bowls.forEach(function (bowl) { 
+        let bowlContent = `
+        <div id="bowl-${bowl.id}" class="${dishDiv}">
+        <div class="${titleDiv}">
+        <h3>${bowl.title}</h3>
+        </div>
+        <div class="${descriptionDiv}">
+            <p>${bowl.description}</p>
+        </div>
+        <div class="${variationDiv}">
+            <p>${bowl.variation}</p>
+        </div>
+        <div class="${priceDiv}">
+            <p>${bowl.price}€</p>
+        </div>
+        <div class="${btnDiv}">
+            <img class="${plusImg}" src="./img/plus.png">
+        </div>
+        </div>
+        `;
+        document.getElementById('bowls-div').insertAdjacentHTML("beforeend", bowlContent);
     });
 }
+
+
+       
--- menu.js
+++ menu.js
@@ -1,8 +1,26 @@
 
 let popular  = [
     {
-        title: '', description: '' 
-    }
+        title: 'Mayan Avocado Salad', 
+        description: 'Fresh salad with mixed vegetables and lemon juiced avocado',
+        variation: 'Variatons: Chipotle-tomato sauce, Habanero sauce, more avoacado',
+        price: '6,99',
+        id: '36'
+    },
+    {
+        title: 'Beef Burrito', 
+        description: 'Grilled Tortilla with grilled Beef and mixed vegetables.',
+        variation: 'Variatons: Chipotle-tomato sauce, Habanero sauce, Avocado sauce, more meat',
+        price: '7,99',
+        id: '37'
+    },
+    {
+        title: 'Pulled Pork Burrito Bowl', 
+        description: 'Grilled Beef, mixed vegetables and rice.',
+        variation: 'Variatons: Chipotle-tomato sauce, Habanero sauce, Avocado sauce, more meat',
+        price: '7,49',
+        id: '38'
+    },
 ];
 
 let salads = [
@@ -45,36 +63,213 @@ let salads = [
 
 let burritos = [
     {
-        title: 'Mexican Beef Salad', 
-        description: 'Fresh salad with mixed vegetables and juicy slices of Beef.',
+        title: 'Chicken Burrito', 
+        description: 'Grilled Tortilla with grilled Chicken and mixed vegetables.',
         variation: 'Variatons: Chipotle-tomato sauce, Habanero sauce, Avocado sauce, more meat',
-        price: '6,99'
+        price: '7,49',
+        id: '1'
+    },
+    {
+        title: 'Beef Burrito', 
+        description: 'Grilled Tortilla with grilled Beef and mixed vegetables.',
+        variation: 'Variatons: Chipotle-tomato sauce, Habanero sauce, Avocado sauce, more meat',
+        price: '7,99',
+        id: '2'
+    },
+    {
+        title: 'Pulled Pork Burrito', 
+        description: 'Grilled Tortilla with pulled pork and mixed vegetables.',
+        variation: 'Variatons: Chipotle-tomato sauce, Habanero sauce, Avocado sauce, more meat',
+        price: '7,49',
+        id: '3'
+    },
+    {
+        title: 'Veggie Burrito', 
+        description: 'Grilled Cheese with pulled pork and mixed vegetables.',
+        variation: 'Variatons: Chipotle-tomato sauce, Habanero sauce, Avocado sauce, more meat',
+        price: '6,99',
+        id: '4'
+    },
+    {
+        title: 'Vegan Burrito', 
+        description: 'Grilled Tortilla with grilled vegetables.',
+        variation: 'Variatons: Chipotle-tomato sauce, Habanero sauce, Avocado sauce, more meat',
+        price: '5,99',
+        id: '5'
     }
 ];
 
 let bowls = [
     {
-        title: 'Mayan Avocado Salad', 
-        description: 'Fresh salad with mixed vegetables and lemon juiced avocado',
-        variation: 'Variatons: Chipotle-tomato sauce, Habanero sauce, more avoacado',
-        price: '6,99'
+        title: 'Chicken Burrito Bowl', 
+        description: 'Grilled Chicken, mixed vegetables and rice.',
+        variation: 'Variatons: Chipotle-tomato sauce, Habanero sauce, Avocado sauce, more meat',
+        price: '7,49',
+        id: '1'
+    },
+    {
+        title: 'Beef Burrito Bowl', 
+        description: 'Grilled Beef, mixed vegetables and rice.',
+        variation: 'Variatons: Chipotle-tomato sauce, Habanero sauce, Avocado sauce, more meat',
+        price: '7,99',
+        id: '2'
+    },
+    {
+        title: 'Pulled Pork Burrito Bowl', 
+        description: 'Grilled Beef, mixed vegetables and rice.',
+        variation: 'Variatons: Chipotle-tomato sauce, Habanero sauce, Avocado sauce, more meat',
+        price: '7,49',
+        id: '3'
+    },
+    {
+        title: 'Veggie Bowl', 
+        description: 'Grilled Cheese, mixed vegetables and rice.',
+        variation: 'Variatons: Chipotle-tomato sauce, Habanero sauce, Avocado sauce, more meat',
+        price: '6,99',
+        id: '4'
+    },
+    {
+        title: 'Vegan Bowl', 
+        description: 'Mixed vegetables, Beans and rice.',
+        variation: 'Variatons: Chipotle-tomato sauce, Habanero sauce, Avocado sauce, more meat',
+        price: '5,99',
+        id: '5'
     }
 ];
 
 let vegan = [
     {
-        title: '', description: '' 
+        title: 'Vegan Salad', 
+        description: 'Fresh salad with mixed vegetables',
+        variation: 'Variatons: Chipotle-tomato sauce, Habanero sauce, add avoacado',
+        price: '5,99',
+        id: '16'
+    },
+    {
+        title: 'Vegan Burrito', 
+        description: 'Grilled Tortilla with grilled vegetables.',
+        variation: 'Variatons: Chipotle-tomato sauce, Habanero sauce, Avocado sauce, more meat',
+        price: '5,99',
+        id: '17'
+    },
+    {
+        title: 'Vegan Bowl', 
+        description: 'Mixed vegetables, Beans and rice.',
+        variation: 'Variatons: Chipotle-tomato sauce, Habanero sauce, Avocado sauce, more meat',
+        price: '6,99',
+        id: '18'
     }
+];
+
+let siders = [
+    {
+        title: 'Tortilla Chips Nature', 
+        price: '1,49',
+        id: '19'
+    },
+    {
+        title: 'Cheesy Tortilla Chips', 
+        price: '2,99'
+        ,
+        id: '20'
+    },
+    {
+        title: 'Tortilla Chips with Creme', 
+        price: '2,99',
+        id: '21'
+    },
+    {
+        title: 'Tortilla Chips with Guacemole', 
+        price: '2,99',
+        id: '21'
+    },
+    {
+        title: 'Tortilla Chips with Classic Salsa', 
+        price: '2,99',
+        id: '22'
+    },
+    {
+        title: 'Tortilla Chips with Chili Salsa', 
+        price: '2,99',
+        id: '23'
+    },
 ];
 
 let desserts = [
     {
-        title: '', description: '' 
+        title: 'Texas Brownie', 
+        price: '3,99',
+        id: '24',
+    },
+    {
+        title: 'Mayan Muffin', 
+        price: '3,99',
+        id: '24'
     }
 ];
 
 let drinks = [
     {
-        title: '', description: '' 
-    }
+        title: 'Coca Cola 1L', 
+        price: '2,99',
+        id: '25'
+    },
+    {
+        title: 'Fanta 1L', 
+        price: '2,99',
+        id: '26'
+    },
+    {
+        title: 'Sprite 1L', 
+        price: '2,99',
+        id: '27'
+    },
+    {
+        title: 'Bonaqua 1L', 
+        price: '1,99',
+        id: '28'
+    },
+    {
+        title: 'Ice Tea Peach 1L', 
+        price: '2,99',
+        id: '29'
+    },
+    {
+        title: 'Ice Tea Lemon 1L', 
+        price: '2,99',
+        id: '29'
+    },
+    {
+        title: 'Mountain Dew 1L', 
+        price: '2,99',
+        id: '30'
+    },
+    {
+        title: 'Club Mate', 
+        price: '1,99',
+        id: '31'
+    },
+    {
+        title: 'Housemade Soda', 
+        price: '2,99',
+        id: '32'
+    },
+];
+
+let alcohol = [
+    {
+        title: 'Warsteiner 0.5L', 
+        price: '2,99',
+        id: '33',
+    },
+    {
+        title: 'Becks 0.5L', 
+        price: '2,99',
+        id: '34',
+    },
+    {
+        title: 'Becks Green Lemon 0.5L', 
+        price: '2,99',
+        id: '35',
+    },
 ];
